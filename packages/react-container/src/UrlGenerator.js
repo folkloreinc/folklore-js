b09166000646d52071d0c6cc8ed8a6924bdc2eec
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-06-13 13:54:40 -0400

    Fix with return in params parsing

--- packages/react-container/src/UrlGenerator.js
+++ packages/react-container/src/UrlGenerator.js
@@ -4,7 +4,7 @@ class UrlGenerator {
     constructor(routes, opts) {
         this.routes = routes;
         this.options = {
-            paramFormat: '\:{key}(\([^\)]+\))?', // eslint-disable-line
+            paramFormat: '\:{key}(\([^\)]*\))?', // eslint-disable-line
             ...opts,
         };
     }
@@ -17,10 +17,9 @@ class UrlGenerator {
         };
         const { withHost, paramFormat, ...params } = options;
         const route = this.routes[key] || key;
-        const url = reduce(params || {}, (str, v, k) => {
-            const paramRegEx = new RegExp(paramFormat.replace(/\{\s*key\s*\}/gi, k), 'i');
-            str.replace(paramRegEx, v);
-        }, route);
+        const url = reduce(params || {}, (str, v, k) => (
+            str.replace(new RegExp(paramFormat.replace(/\{\s*key\s*\}/gi, k), 'i'), v)
+        ), route);
 
         const host = typeof window !== 'undefined' ? `${window.location.protocol}//${window.location.host}` : '';
         const hostPattern = host !== null ? `^${host}` : '^$';
