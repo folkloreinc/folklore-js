e4ee24c131dd484cf112998ec3ae7cd48b32b043
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-07-10 11:45:29 -0400

    Updated jest config

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    setupFiles: [
+        '<rootDir>/__tests__/shim.js',
+        '<rootDir>/__tests__/setup.js',
+    ],
+    testPathIgnorePatterns: [
+        '<rootDir>/packages/[^/]+/lib/',
+        '<rootDir>/packages/[^/]+/es/',
+        '<rootDir>/packages/[^/]+/dist/',
+        '<rootDir>/packages/generator-folklore/lib/',
+        '<rootDir>/packages/generator-folklore/src/generators/[^/]+/templates/',
+        '<rootDir>/__tests__/shim.js',
+        '<rootDir>/__tests__/setup.js',
+        '<rootDir>/__tests__/storyshots.test.js',
+    ],
+    modulePathIgnorePatterns: [
+        '<rootDir>/packages/[^/]+/lib/',
+        '<rootDir>/packages/[^/]+/es/',
+        '<rootDir>/packages/[^/]+/dist/',
+        '<rootDir>/packages/generator-folklore/lib/generators/[^/]+/templates/',
+        '<rootDir>/packages/generator-folklore/src/generators/[^/]+/templates/',
+    ],
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
+        '^@folklore/(.*)': '<rootDir>/packages/$1/src/index.js',
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'packages/*/src/**/*.{js,jsx}',
+        '!packages/*/src/generators/*/templates/**',
+        '!**/node_modules/**',
+        '!**/vendor/**',
+        '!**/__tests__/**',
+        '!**/__stories__/**',
+    ],
+};
