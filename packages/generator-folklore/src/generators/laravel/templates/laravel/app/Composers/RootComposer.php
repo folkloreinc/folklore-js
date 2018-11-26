cf08eb6d1d35bc3882cd8685a54cef15b85ebd62
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-11-26 11:51:26 -0500

    Added patterns to routes

--- packages/generator-folklore/src/generators/laravel/templates/laravel/app/Composers/RootComposer.php
+++ packages/generator-folklore/src/generators/laravel/templates/laravel/app/Composers/RootComposer.php
@@ -79,6 +79,7 @@ class RootComposer
             $routeNames = $this->getRoutesNames();
         }
 
+        $patterns = $this->router->getPatterns();
         foreach ($routeNames as $name) {
             $route = $allRoutes->getByName($name);
             $parameters = $route->parameterNames();
@@ -87,8 +88,14 @@ class RootComposer
             foreach ($parameters as $parameter) {
                 $params[] = ':'.$parameter;
             }
-            $routes[$key] = route($name, $params, false);
-            $routes[$name] = $routes[$key];
+            $path = route($name, $params, false);
+            foreach ($parameters as $parameter) {
+                if (isset($patterns[$parameter])) {
+                    $pattern = preg_replace('/^\(?(.*?)\)?$/', '$1', $patterns[$parameter]);
+                    $path = preg_replace('/('.preg_quote(':'.$parameter).')\b/i', '$1('.$pattern.')', $path);
+                }
+            }
+            $routes[$key] = $routes[$name] = $path;
         }
 
         return $routes;
