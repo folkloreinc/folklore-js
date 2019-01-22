8774b6596524096e4a3543515c95fe0e620b450c
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2019-01-22 15:32:03 -0500

    Splitted logic in composers

--- packages/generator-folklore/src/generators/laravel/templates/laravel/app/Composers/RootComposer.php
+++ packages/generator-folklore/src/generators/laravel/templates/laravel/app/Composers/RootComposer.php
@@ -4,112 +4,28 @@ namespace App\Composers;
 
 use Illuminate\View\View;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Router;
 use Illuminate\Contracts\View\Factory;
 use StdClass;
 
 class RootComposer
 {
-    protected $defaultRoutes = [
-
-    ];
-
-    protected $localizedRoutes = [
-        'home',
-        'test',
-        'test_with_param'
-    ];
-
-    protected $translations = [
-        'meta',
-        'content',
-    ];
-
     protected $request = null;
-    protected $router = null;
 
-    public function __construct(Request $request, Router $router)
+    public function __construct(Request $request)
     {
         $this->request = $request;
-        $this->router = $router;
     }
 
     public function compose(View $view)
     {
         $path = '/'.ltrim($this->request->path(), '/');
         $locale = isset($view->locale) ? $view->locale : app()->getLocale();
-        $routes = $this->getRoutes($locale, isset($view->routes) ? $view->routes : null);
-        $translations = isset($view->translations) ? $view->translations : $this->getTranslations($locale);
-
-        $view->props = [
+        $viewProps = is_array($view->props) ? $view->props : [];
+        $view->props = array_merge($viewProps, [
             'url' => $path,
-            'routes' => $routes,
             'locale' => $locale,
-            'messages' => $translations,
-        ];
-    }
-
-    protected function getTranslations($locale)
-    {
-        $translations = [];
-        $translations[$locale] = [];
-        foreach ($this->translations as $translation) {
-            $texts = trans($translation, [], $locale);
-            if (is_null($texts)) {
-                continue;
-            }
-            $texts = is_string($texts) ? [$texts] : array_dot($texts);
-            foreach ($texts as $key => $value) {
-                $key = sizeof($texts) === 1 && $key === 0 ? $translation : ($translation.'.'.$key);
-                $translations[$locale][$key] = preg_replace(
-                    '/\:([a-z][a-z0-9\_\-]+)/',
-                    '{$1}',
-                    $value
-                );
-            }
-        }
-        return $translations;
-    }
-
-    protected function getRoutes($locale, $routeNames = null)
-    {
-        $routes = [];
-        $allRoutes = $this->router->getRoutes();
-        if (is_null($routeNames)) {
-            $routeNames = $this->getRoutesNames();
-        }
-
-        $patterns = $this->router->getPatterns();
-        foreach ($routeNames as $name) {
-            $route = $allRoutes->getByName($name);
-            $parameters = $route->parameterNames();
-            $key = preg_replace('/\.'.$locale.'$/', '', $name);
-            $params = [];
-            foreach ($parameters as $parameter) {
-                $params[] = ':'.$parameter;
-            }
-            $path = route($name, $params, false);
-            foreach ($parameters as $parameter) {
-                if (isset($patterns[$parameter])) {
-                    $pattern = preg_replace('/^\(?(.*?)\)?$/', '$1', $patterns[$parameter]);
-                    $path = preg_replace('/('.preg_quote(':'.$parameter).')\b/i', '$1('.$pattern.')', $path);
-                }
-            }
-            $routes[$key] = $routes[$name] = $path;
-        }
-
-        return $routes;
-    }
-
-    protected function getRoutesNames()
-    {
-        $locales = config('locale.locales');
-        $routes = $this->defaultRoutes;
-        foreach ($this->localizedRoutes as $routeName) {
-            foreach ($locales as $locale) {
-                $routes[] = $routeName.'.'.$locale;
-            }
-        }
-        return $routes;
+            'routes' => $view->routes,
+            'messages' => $view->translations,
+        ]);
     }
 }
--- packages/generator-folklore/src/generators/laravel/templates/laravel/app/Providers/ViewServiceProvider.php
+++ packages/generator-folklore/src/generators/laravel/templates/laravel/app/Providers/ViewServiceProvider.php
@@ -15,7 +15,23 @@ class ViewServiceProvider extends ServiceProvider
     {
         $view = $this->app['view'];
         $view->composer('layouts.main', \App\Composers\Layouts\MainComposer::class);
-        $view->composer(['root'], \App\Composers\RootComposer::class);
+
+        $view->composer('errors::404', function (View $view) {
+            $view->with('props', [
+                'statusCode' => 404,
+            ]);
+        });
+
+        $view->composer('errors::500', function (View $view) {
+            $view->with('props', [
+                'statusCode' => 500,
+            ]);
+        });
+
+        $reactPages = ['page', 'errors::404', 'errors::500'];
+        $view->composer($reactPages, \App\Composers\TranslationsComposer::class);
+        $view->composer($reactPages, \App\Composers\RoutesComposer::class);
+        $view->composer($reactPages, \App\Composers\RootComposer::class);
     }
 
     /**
