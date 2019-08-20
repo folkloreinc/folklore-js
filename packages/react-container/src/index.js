12e47bc3f0a56ca7434b712e80eb6d71f62ed1e2
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2019-08-20 13:12:09 -0400

    Updated react-container to exclude third parties providers

--- packages/react-container/src/index.js
+++ packages/react-container/src/index.js
@@ -1,21 +1,3 @@
-import Container from './Container';
-import IntlContainer from './IntlContainer';
-import RouterContainer from './RouterContainer';
-import StoreContainer from './StoreContainer';
-import configureStore from './configureStore';
-import UrlGenerator from './UrlGenerator';
-import withUrlGenerator from './withUrlGenerator';
-import withUrlGeneratorMiddleware from './withUrlGeneratorMiddleware';
-
-export {
-    Container,
-    IntlContainer,
-    RouterContainer,
-    StoreContainer,
-    configureStore,
-    UrlGenerator,
-    withUrlGenerator,
-    withUrlGeneratorMiddleware,
-};
-
-export default Container;
+export { default as createStore } from './createStore';
+export { default as UrlGenerator } from './UrlGenerator';
+export * from './contexts/UrlGenerator';
