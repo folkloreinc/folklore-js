651973dd3552d19c5493d7fa076e2f4fbc7c5da0
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2021-12-02 16:58:20 -0500

    Fix plugin

--- packages/cli/src/imageminPresets.js
+++ packages/cli/src/imageminPresets.js
@@ -40,7 +40,10 @@ export default {
                 'svgo',
                 {
                     plugins: [
-                        { removeViewBox: false },
+                        {
+                            name: "removeViewBox",
+                            active: false,
+                          },
                         {
                             name: 'addAttributesToSVGElement',
                             params: {
