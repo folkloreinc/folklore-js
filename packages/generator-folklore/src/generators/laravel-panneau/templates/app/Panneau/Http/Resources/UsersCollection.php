14706e7d5f3769501ffb018281f6866d895c3800
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2022-08-26 10:14:26 -0400

    Fix with collection

--- packages/generator-folklore/src/generators/laravel-panneau/templates/app/Panneau/Http/Resources/UsersCollection.php
+++ packages/generator-folklore/src/generators/laravel-panneau/templates/app/Panneau/Http/Resources/UsersCollection.php
@@ -4,7 +4,7 @@ namespace App\Panneau\Http\Resources;
 
 use Folklore\Http\Resources\Collection;
 
-class BlocksCollection extends Collection
+class UsersCollection extends Collection
 {
-    public $collects = BlockResource::class;
+    public $collects = UserResource::class;
 }
