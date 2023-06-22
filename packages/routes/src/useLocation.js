938a00a9a5d73d7173160d43c9744c06d4c54e7f
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2023-06-22 13:32:51 -0400

    fix with url

--- packages/routes/src/useLocation.js
+++ packages/routes/src/useLocation.js
@@ -1,8 +1,15 @@
 import { useMemo } from 'react';
 import { useLocation as useLocationBase } from 'wouter';
 
-export default function useLocation () {
+export default function useLocation(baseUrl) {
     const [location, setLocation] = useLocationBase();
-    const url = useMemo(() => new URL(location || '/'), [location]);
+    const url = useMemo(
+        () =>
+            new URL(
+                location || '/',
+                baseUrl || `${window.location.protocol}//${window.location.host}`,
+            ),
+        [baseUrl, location],
+    );
     return [url, setLocation];
 }
