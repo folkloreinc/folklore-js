adcd48a87fdc5d6d56e06f4250b6a832d0b570ac
Nicolas Roy B <phatshambler@gmail.com>
2023-07-10 16:21:36 -0400

    Wouter location fix

--- packages/routes/src/useLocation.js
+++ packages/routes/src/useLocation.js
@@ -1,10 +1,12 @@
 import { useMemo } from 'react';
 import { useLocation as useLocationBase } from 'wouter';
+import { useSearch as useSearchBase } from 'wouter/use-location';
 
 import parseLocation from './parseLocation';
 
 export default function useLocation() {
     const [location, setLocation] = useLocationBase();
-    const url = useMemo(() => parseLocation(location), [location]);
+    const search = useSearchBase();
+    const url = useMemo(() => parseLocation(location, search), [location, search]);
     return [url, setLocation];
 }
