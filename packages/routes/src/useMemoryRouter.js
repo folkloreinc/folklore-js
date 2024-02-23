1cf69ff746978b91ab7174870d37cadff78db47e
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2024-02-23 12:49:20 -0500

    Fix with searchHook

--- packages/routes/src/useMemoryRouter.js
+++ packages/routes/src/useMemoryRouter.js
@@ -44,7 +44,7 @@ export default function useMemoryRouter({
         useSyncExternalStore(subscribe, () => currentPath.pathname),
         navigate,
     ];
-    const searchHook = () => [useSyncExternalStore(subscribe, () => currentPath.search), navigate];
+    const searchHook = () => useSyncExternalStore(subscribe, () => currentPath.search);
 
     return {
         hook: locationHook,
