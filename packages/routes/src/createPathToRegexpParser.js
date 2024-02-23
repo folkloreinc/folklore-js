8b2b73c9e314ff104c9fc734f3a3af5914b86aac
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2024-02-23 10:43:44 -0500

    Moved to wouter 3

---
+++ packages/routes/src/createPathToRegexpParser.js
@@ -0,0 +1,15 @@
+import { pathToRegexp } from 'path-to-regexp';
+
+export default function createPathToRegexpParser(opts) {
+    return (path, loose) => {
+        const keys = [];
+        const pattern = pathToRegexp(path, keys, { strict: false, end: !loose, ...opts });
+
+        return {
+            pattern,
+            // `pathToRegexp` returns some metadata about the keys,
+            // we want to strip it to just an array of keys
+            keys: keys.map((k) => k.name),
+        };
+    };
+}
--- packages/routes/src/index.js
+++ packages/routes/src/index.js
@@ -5,4 +5,4 @@ export { default as useUrlGenerator } from './useUrlGenerator';
 export { default as generatePath } from './generatePath';
 export { default as useLocation } from './useLocation';
 export { default as useMemoryLocationHook } from './useMemoryLocationHook';
-export { default as createPathToRegexpMatcher } from './createPathToRegexpMatcher';
+export { default as createPathToRegexpParser } from './createPathToRegexpParser';
