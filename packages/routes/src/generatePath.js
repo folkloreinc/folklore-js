76a7f02d9e6a887f3f11d269a45b62136397596e
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2024-02-23 16:32:59 -0500

    Fix with generate path

--- packages/routes/src/generatePath.js
+++ packages/routes/src/generatePath.js
@@ -1,5 +1,11 @@
-import { inject } from 'regexparam';
-
 export default function generatePath(path, data) {
-    return inject(path, data);
+    const RGX = /(\/|^)([:*][^/]*?)(\?)?(?=[/.]|$)/g;
+
+    return path.replace(RGX, (x, lead, key, optional) => {
+        const newX = data[key === '*' ? key : key.replace(/^:([^(]+)(?=\()?.*$/, '$1')] || null;
+        if (newX !== null) {
+            return `/${newX}`;
+        }
+        return optional || key === '*' ? '' : `/${key}`;
+    });
 }
