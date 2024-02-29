4ea4d49bfefdaf7c7aa1eb21af9447bddb8488dc
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2024-02-29 13:22:08 -0500

    Fix with generate path

--- packages/routes/src/generatePath.js
+++ packages/routes/src/generatePath.js
@@ -3,12 +3,10 @@ import { compile } from 'path-to-regexp';
 const compilers = {};
 
 export default function generatePath(fullPath, data, opts = {}) {
-    const fullUrlMatches = fullPath.match(/^(https?:\/\/[^/]+)\/?/);
+    const fullUrlMatches = fullPath.match(/^(https?:\/\/)/);
     if (typeof compilers[fullPath] === 'undefined') {
-        compilers[fullPath] = compile(fullPath.replace(/^(https?:\/\/[^/]+)\/?/, '/'), opts);
+        compilers[fullPath] = compile(fullPath.replace(/^(https?:\/\/)/, ''), opts);
     }
     const compiler = compilers[fullPath];
-    return fullUrlMatches !== null
-        ? `${fullUrlMatches[1].substr(0, -1)}${compiler(data)}`
-        : compiler(data);
+    return fullUrlMatches !== null ? `${fullUrlMatches[1]}${compiler(data)}` : compiler(data);
 }
