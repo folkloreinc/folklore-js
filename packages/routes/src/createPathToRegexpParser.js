f2b2be214c5cdabed0e31068f3f02f3d43febdd2
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2024-02-29 11:36:53 -0500

    Fix

--- packages/routes/src/createPathToRegexpParser.js
+++ packages/routes/src/createPathToRegexpParser.js
@@ -2,7 +2,7 @@ import { pathToRegexp } from 'path-to-regexp';
 
 export default function createPathToRegexpParser(opts) {
     return (fullPath, loose) => {
-        const path = fullPath.replace(/^(https?:\/\/[^/]+)\//, '/');
+        const path = fullPath.replace(/^(https?:\/\/[^/]+)\/?/, '/');
         const keys = [];
         const isWildcard = path.match(/(\/|^)\*$/) !== null;
         const pattern = pathToRegexp(isWildcard ? path.replace(/(\/|^)\*$/, '$1(.*)') : path, keys, {
--- packages/routes/src/generatePath.js
+++ packages/routes/src/generatePath.js
@@ -3,9 +3,9 @@ import { compile } from 'path-to-regexp';
 const compilers = {};
 
 export default function generatePath(fullPath, data, opts = {}) {
-    const fullUrlMatches = fullPath.match(/^(https?:\/\/[^/]+)\//);
+    const fullUrlMatches = fullPath.match(/^(https?:\/\/[^/]+)\/?/);
     if (typeof compilers[fullPath] === 'undefined') {
-        compilers[fullPath] = compile(fullPath.replace(/^(https?:\/\/[^/]+)\//, '/'), opts);
+        compilers[fullPath] = compile(fullPath.replace(/^(https?:\/\/[^/]+)\/?/, '/'), opts);
     }
     const compiler = compilers[fullPath];
     return fullUrlMatches !== null
