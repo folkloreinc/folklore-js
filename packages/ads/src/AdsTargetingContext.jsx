cd8d325c3aa30d11f77c3a61e540039d127dff00
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2024-06-14 09:10:47 -0400

    Fix

--- packages/ads/src/AdsTargetingContext.jsx
+++ packages/ads/src/AdsTargetingContext.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 
 const AdsTargetingContext = React.createContext(null);
 
@@ -9,6 +9,7 @@ const propTypes = {
     children: PropTypes.node.isRequired,
     // eslint-disable-next-line react/forbid-prop-types
     targeting: PropTypes.object,
+    replace: PropTypes.bool,
 };
 
 const defaultProps = {
@@ -18,11 +19,19 @@ const defaultProps = {
                 ? `${window.location.protocol}//${window.location.host}`
                 : null,
     },
+    replace: false,
 };
 
-export function AdsTargetingProvider({ children, targeting }) {
+export function AdsTargetingProvider({ children, targeting, replace }) {
+    const previousTargeting = useAdsTargeting();
+    const mergedTargeting = useMemo(
+        () => (replace ? targeting : { ...previousTargeting, ...targeting }),
+        [replace, previousTargeting, targeting],
+    );
     return (
-        <AdsTargetingContext.Provider value={targeting}>{children}</AdsTargetingContext.Provider>
+        <AdsTargetingContext.Provider value={mergedTargeting}>
+            {children}
+        </AdsTargetingContext.Provider>
     );
 }
 
