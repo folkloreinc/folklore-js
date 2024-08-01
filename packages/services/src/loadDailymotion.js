9f4bc21587b8f4af811238f9fcd28374316ef328
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2024-08-01 14:19:40 -0400

    Fix

--- packages/services/src/loadDailymotion.js
+++ packages/services/src/loadDailymotion.js
@@ -5,7 +5,7 @@ import loadScriptWithCallback from './loadScriptWithCallback';
 const loadDailymotion = createLoader(
     ({ url = 'https://api.dmcdn.net/all.js', callback = 'dmAsyncInit' } = {}) =>
         callback !== null ? loadScriptWithCallback(url, callback) : loadScript(url),
-    () => (typeof window.DM !== 'undefined' ? window.DM : null),
+    () => (typeof window !== 'undefined' ? window.DM || window.dailymotion || null : null),
 );
 
 export default loadDailymotion;
