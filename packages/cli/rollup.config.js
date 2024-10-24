b87a694b683f7ba8dceac3c2cd7284ab3b5035e8
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2024-10-24 13:52:53 -0400

    Fix

--- packages/cli/rollup.config.js
+++ packages/cli/rollup.config.js
@@ -9,15 +9,17 @@ export default [
             file: 'dist/cli.js',
             format: 'cjs',
             banner: '#!/usr/bin/env node',
+            // inlineDynamicImports: true
         },
         plugins: [
-            resolve({
-                // exportConditions: ['node'],
-                // modulesOnly: true,
-                resolveOnly: [
-                    'chalk',
-                ],
-            }),
+            // resolve({
+            //     // exportConditions: ['node'],
+            //     // modulesOnly: true,
+            //     resolveOnly: [
+            //         'chalk',
+            //         '@formatjs/cli-lib'
+            //     ],
+            // }),
             ...plugins,
         ],
     },
