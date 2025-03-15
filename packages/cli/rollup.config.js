41dde0b5c9f5f3dbb242d68180ebfbf5975e6c22
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2025-03-15 13:38:56 -0400

    Fix

--- packages/cli/rollup.config.js
+++ packages/cli/rollup.config.js
@@ -26,6 +26,7 @@ export default [
                 resolveOnly: [
                     /@formatjs/,
                     /@vue/,
+                    /vue\//,
                 ],
             }),
             ...plugins,
