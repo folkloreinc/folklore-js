50b94113cd23ee1fd8f6c782dac4fd4c55820286
Nicolas Roy B. <phatshambler@gmail.com>
2026-08-27 16:04:59 -0400

    fix

--- packages/generator-folklore/src/generators/react-app/templates/src/components/pages/Error.tsx
+++ packages/generator-folklore/src/generators/react-app/templates/src/components/pages/Error.tsx
@@ -10,58 +10,71 @@ export const messages = defineMessages({
     metaTitle401: {
         id: 'meta.title_401',
         defaultMessage: 'Error 401',
+        description: 'Page title',
     },
     title401: {
         id: 'errors.title_401',
         defaultMessage: 'Error 401',
+        description: 'Page title',
     },
     description401: {
         id: 'errors.description_401',
         defaultMessage: 'You are not authorized to access this page.',
+        description: 'Page description',
     },
 
     metaTitle403: {
         id: 'meta.title_403',
         defaultMessage: 'Error 403',
+        description: 'Page title',
     },
     title403: {
         id: 'errors.title_403',
         defaultMessage: 'Error 403',
+        description: 'Page title',
     },
     description403: {
         id: 'errors.description_403',
         defaultMessage: 'Access to this page is forbidden',
+        description: 'Page description',
     },
 
     metaTitle404: {
         id: 'meta.title_404',
         defaultMessage: 'Error 404',
+        description: 'Page title',
     },
     title404: {
         id: 'errors.title_404',
         defaultMessage: 'Error 404',
+        description: 'Page title',
     },
     description404: {
         id: 'errors.description_404',
         defaultMessage: 'This page doesn’t exists',
+        description: 'Page description',
     },
 
     metaTitle500: {
         id: 'meta.title_500',
         defaultMessage: 'Error 500',
+        description: 'Page title',
     },
     title500: {
         id: 'errors.title_500',
         defaultMessage: 'Error 500',
+        description: 'Page title',
     },
     description500: {
         id: 'errors.description_500',
         defaultMessage: 'There was an error',
+        description: 'Page description',
     },
 
     gotoHome: {
         id: 'errors.goto_home',
         defaultMessage: 'Go to home',
+        description: 'Link label',
     },
 });
 
