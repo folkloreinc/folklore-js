52c21287d4427ee2f964f8af30023d897b7c3a61
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2026-09-03 09:22:52 -0400

    Added lossy preset

--- packages/cli/src/imageminPresets.js
+++ packages/cli/src/imageminPresets.js
@@ -1,31 +1,4 @@
-// import imageminGifsicle from 'imagemin-gifsicle';
-// import imageminJpegtran from 'imagemin-jpegtran';
-// import imageminMozjpeg from 'imagemin-mozjpeg';
-// import imageminOptipng from 'imagemin-optipng';
-// import imageminPngquant from 'imagemin-pngquant';
-// import imageminSvgo from 'imagemin-svgo';
-
 export default {
-    // lossless: {
-    //     plugins: [
-    //         imageminGifsicle({
-    //             interlaced: false,
-    //         }),
-    //         imageminJpegtran({ progressive: true }),
-    //         imageminOptipng({ optimizationLevel: 5 }),
-    //         imageminSvgo({
-    //             plugins: [
-    //                 { removeViewBox: false },
-    //                 {
-    //                     name: 'addAttributesToSVGElement',
-    //                     params: {
-    //                         attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
-    //                     },
-    //                 },
-    //             ],
-    //         }),
-    //     ],
-    // },
     lossless: {
         plugins: [
             [
@@ -55,24 +28,28 @@ export default {
             ],
         ],
     },
-    // lossy: {
-    //     plugins: [
-    //         imageminGifsicle({
-    //             interlaced: false,
-    //         }),
-    //         imageminMozjpeg(),
-    //         imageminPngquant(),
-    //         imageminSvgo({
-    //             plugins: [
-    //                 { removeViewBox: false },
-    //                 {
-    //                     name: 'addAttributesToSVGElement',
-    //                     params: {
-    //                         attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
-    //                     },
-    //                 },
-    //             ],
-    //         }),
-    //     ],
-    // },
+    lossy: {
+        plugins: [
+            ['gifsicle', { interlaced: false }],
+            ['mozjpeg', {}],
+            ['pngquant', {}],
+            [
+                'svgo',
+                {
+                    plugins: [
+                        {
+                            name: 'removeViewBox',
+                            active: false,
+                        },
+                        {
+                            name: 'addAttributesToSVGElement',
+                            params: {
+                                attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+                            },
+                        },
+                    ],
+                },
+            ],
+        ],
+    },
 };
